dbbc002277ee63417c071a42e390faea3c4777a5
KTGhosT <sa24610220@my.sliit.lk>
2025-10-20 18:58:39 +0530

    First Commit

---
+++ src/main/java/com/example/fixora/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.fixora.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "👋 Welcome to your Spring Boot Web App!");
+        return "index";
+    }
+}
---
+++ src/test/java/com/example/fixora/FixoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fixora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
